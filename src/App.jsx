1517c089aebd8deabc98286f630f259bb5ba9a32
Ahmad Ali Tresvant <ahmadtresvant@Alis-M1-Pro.local>
2023-11-13 16:40:48 -0500

    finished project and added css

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,30 @@ function App() {
   console.log(useState(puppyList));
 
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const handleClick = () => {
+
+  }
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup);
 
   return (
     <>
       <div className="App">
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
         })}
+         {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
     </>
   );
