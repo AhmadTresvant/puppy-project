af793162e46d684288698c63317bf4089405a534
Ahmad Ali Tresvant <ahmadtresvant@Alis-M1-Pro.local>
2023-11-13 15:46:02 -0500

    added all puppy names

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-
-import './App.css'
+import { puppyList } from "./data.js";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  console.log(useState(puppyList));
 
+  const [puppies, setPuppies] = useState(puppyList);
 
   return (
     <>
-      <div>
-   
+      <div className="App">
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })}
       </div>
-   
     </>
-  )
+  );
 }
 
-export default App
+export default App;
